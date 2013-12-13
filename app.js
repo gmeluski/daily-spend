d26418ff28be4871ea913ec63571fd4c7e5ccc2a
gmeluski <gmeluski@gmail.com>
2013-12-13 16:49:33 -0500

    adding in new fiels

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/spend', routes.spend);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
