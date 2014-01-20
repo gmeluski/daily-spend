8f89d16dcc5774a699bb46a743bda500905554a2
Gregg Meluski <gmeluski@hotmail.com>
2014-01-20 15:55:46 -0500

    adding in the proper date stuff

--- app/models/spend.js
+++ app/models/spend.js
@@ -2,16 +2,9 @@ var MongoClient = require('mongodb').MongoClient, Server = require('mongodb').Se
 var mongoClient = new MongoClient(new Server('localhost', 27017));
 
 module.exports = {
- 
-    testYo: function () {
-        
-        console.log('out');    
-    },
-
     returnCollection: function () {
         var db = mongoClient.db('expensesTest')
         var collection = db.collection('dailySpend'); 
-
         return collection;
     },
 
@@ -20,41 +13,52 @@ module.exports = {
         var collection = db.collection('dailySpend'); 
         var insertObject = {
                 userId: 1,
-                createdOn: new Date(),
+                createdOn: new Date().toISOString(),
                 amount: parseFloat(amount)
             };
 
         mongoClient.open(function(err, mongoClient) {
-            
             collection.insert(insertObject, function () {
                 mongoClient.close();    
             });
         });   
     },
 
+    returnStartOfDay: function (desiredDate) {
+        var newDayHours = 'T00:00:00.000Z';
+        var properMonth = ("0" + (desiredDate.getMonth() + 1)).slice(-2);
+        var properDate = ("0" + desiredDate.getDate()).slice(-2)
+        var newDayString = desiredDate.getFullYear() + '-' + properMonth + '-' + properDate + newDayHours;
+        return new Date(newDayString).toISOString(); 
+    },
+
 
     aggregateExpenses: function () {
         var start = new Date(2014, 1, 1);
+
         var end = new Date(2014, 2, 1);
         var spendModel = this;
-        // db.dailySpend.find({createdOn: {$gte: ISODate('2010-01-01T00:00:00.000Z')}})
-        // db.dailySpend.aggregate({ $match: { createdOn: { $gte: ISODate('2010-01-01T00:00:00.000Z')}}}, {$group: {_id: '0', sum: {$sum: '$amount'} }}) 
-        var aggregateList = [{ $match: { createdOn: { $gte: new Date('2010-01-01T00:00:00.000Z').toISOString()}}}, {$group: {_id: '0', sum: {$sum: '$amount'} }}];
+        var startToday = this.returnStartOfDay(new Date());
+        
+        //var startToday = new Date(todaysDate.getFullYear() + '-' + todaysDate.getMonth() + '-' + todaysDate.getDate() + startString);
+
+        var testDate = new Date('2010-01-01T00:00:00.000Z').toISOString();
+
+        console.log(startToday);
+        console.log(testDate);
+        var matchObject = { $match: { createdOn: { $gte: new Date('2010-01-01T00:00:00.000Z').toISOString()}}};   
+        var groupObject = {$group: {_id: '0', sum: {$sum: '$amount'} }}
+        var aggregateList = [matchObject, groupObject];
         var db = mongoClient.db('expensesTest')
         var collection = db.collection('dailySpend'); 
         
         mongoClient.open(function (err, mongoClient) {
-            collection.aggregate(aggregateList, function (err, result) {
+            collection.aggregate([matchObject], function (err, result) {
+                console.log('simpleMatch');
+                console.log('-------------------');
                 console.log(result); 
-            
+                mongoClient.close();
             }); 
-            
-            
-            collection.find().toArray(function(e, docs){
-                console.log(docs.length); 
-                
-                mongoClient.close(); 
-            });
         });
 
     }
