6da0f5dfd379c99eabbf98acfa12d9fb41b2129f
Gregg Meluski <gmeluski@hotmail.com>
2014-01-29 08:53:40 -0500

    solving the ISOString issue

--- app/models/spend.js
+++ app/models/spend.js
@@ -11,6 +11,10 @@ module.exports = {
     writeExpense: function (amount) {
         var db = mongoClient.db('expensesTest')
         var collection = db.collection('dailySpend'); 
+        console.log(new Date().getDate());
+        console.log(new Date().toISOString());
+        // why is ISOString time farther ahead of getDate at say, 7 o'clock local time. WTF is up with that?
+        
         var insertObject = {
                 userId: 1,
                 createdOn: new Date().toISOString(),
@@ -24,11 +28,22 @@ module.exports = {
         });   
     },
 
-    getStartOfDay: function (desiredDate) {
-        var newDayHours = 'T00:00:00.000Z';
+    getTimeString: function(date) {
+        return 'T' + this.padNumber(date.getHours()) + ':' + this.padNumber(date.getMinutes()) + ':' + this.padNumber(date.getSeconds) + '.' + date.getMilliseconds() +'Z';
+    },
+
+    padNumber: function (number) { 
+        return ("0" + number).slice(-2); 
+    },
+
+    getProperTime: function (desiredDate, desiredTime) {
         var properMonth = ("0" + (desiredDate.getMonth() + 1)).slice(-2);
         var properDate = ("0" + desiredDate.getDate()).slice(-2)
-        var newDayString = desiredDate.getFullYear() + '-' + properMonth + '-' + properDate + newDayHours;
+        return desiredDate.getFullYear() + '-' + properMonth + '-' + properDate + desiredTime;
+    },
+
+    getStartOfDay: function (desiredDate) {
+        var newDayString = this.getProperTime(desiredDate, 'T00:00:00.000Z');
         return new Date(newDayString).toISOString(); 
     },
 
@@ -44,7 +59,7 @@ module.exports = {
     aggregateExpenses: function () {
         var spendModel = this;
 
-        var matchObject = { $match: { createdOn: spendModel.getDayRange() }};   
+        var matchObject = { $match: { createdOn: spendModel.getDayRange() }};  
         var groupObject = {$group: {_id: '0', sum: {$sum: '$amount'} }}
         var db = mongoClient.db('expensesTest')
         var collection = db.collection('dailySpend'); 
--- app/test/test.js
+++ app/test/test.js
@@ -2,9 +2,18 @@
 var chai = require('chai');
 var spendModel = require('../models/spend');
 
-describe('testing the spendModel', function (){
+describe('the spendModel', function (){
     it('should return a string for start of day', function() {
         chai.expect(spendModel.getStartOfDay(new Date())).to.be.a('string'); 
+    }); 
+    
+    it('should return a string for the proper time', function() {
+        chai.expect(spendModel.getProperTime(new Date(), 'T00:00:00.000Z')).to.be.a('string'); 
+    }); 
+
+    
+    it('should return a string for time string', function() {
+        chai.expect(spendModel.getTimeString(new Date())).to.be.a('string'); 
     }); 
     
 });
