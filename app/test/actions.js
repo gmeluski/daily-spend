18dcb66ce4cd4d137f1bc81bd87b8e560b669a25
gmeluski <gmeluski@gmail.com>
2014-02-21 17:23:11 -0500

    another step in the actions

--- app/test/actions.js
+++ app/test/actions.js
@@ -1,2 +1,13 @@
 var chai = require('chai');
 var actions = require('../../public/scripts/scripts/actions.js'); 
+
+
+describe('the actions', function (){
+    console.log('hi');
+    console.log(actions);
+
+//    it('should return an object for getDayRange', function() {
+//        chai.expect(spendModel.getDayRange()).to.be.an('object'); 
+//    });
+
+});
